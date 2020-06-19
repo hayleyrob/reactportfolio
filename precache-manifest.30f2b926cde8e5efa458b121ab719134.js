5a9a235f4811ef5117f0231cf33bf00d714c39d8
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-19 14:52:56 -0500

    Updates

---
+++ precache-manifest.30f2b926cde8e5efa458b121ab719134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78648fe09a2da807626eaad7281ca441",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "0048afc31ed3e31a7946",
+    "url": "/reactportfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff06b513f3a0ada4d48c",
+    "url": "/reactportfolio/static/js/2.3e37b063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactportfolio/static/js/2.3e37b063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0048afc31ed3e31a7946",
+    "url": "/reactportfolio/static/js/main.7b0be6e7.chunk.js"
+  },
+  {
+    "revision": "680b805bbde6fe945805",
+    "url": "/reactportfolio/static/js/runtime-main.bd8f0886.js"
+  }
+]);
