74fe32b19562201613ed3cca37c598e25a20f523
Hayley Robinson <hayleyrobinson328@gmail.com>
2020-06-19 17:11:56 -0500

    updated portfolio items

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -36,6 +36,40 @@ const Portfolio = () => {
 
       </div>
     </div>
+    <div className="row">
+      <div className="col-md-4">
+        <div className="card" style={{ maxWidth: "18rem" }}>
+          <img src="https://i.imgur.com/saoSkeo.png" className="card-img-top" alt="eSPACE" />
+          <div className="card-body">
+            <h5 className="card-title">Express NoteTaker</h5>
+            <p className="card-text">An App for creating and accessing notes built with Express and Node</p>
+            <a href="https://nameless-inlet-68056.herokuapp.com/" className="btn btn-primary" target="_blank">Visit App</a>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="card" style={{ maxWidth: "18rem" }}>
+          <img src="https://i.imgur.com/vBPj3VB.png" className="card-img-top" alt="ArtistaDay" />
+          <div className="card-body">
+            <h5 className="card-title">Day Planner</h5>
+            <p className="card-text">A Javascript app for planning your day</p>
+            <a href="https://hayleyrob.github.io/Day-Planner/" className="btn btn-primary" target="_blank">Visit App</a>
+          </div>
+        </div>
+
+      </div>
+      <div className="col-md-4">
+        <div className="card" style={{ maxWidth: "18rem" }}>
+          <img src="https://i.imgur.com/u00v92F.png" className="card-img-top" alt="WorkoutTracker" />
+          <div className="card-body">
+            <h5 className="card-title">Code Quiz</h5>
+            <p className="card-text">A Javascript Quiz App for Coding</p>
+            <a href="https://hayleyrob.github.io/Code-Quiz/" className="btn btn-primary" target="_blank">Visit App</a>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 export default Portfolio
